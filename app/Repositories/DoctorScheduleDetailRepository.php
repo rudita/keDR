e2b19082d0c49afc691b2018196e154971159286
Rudita <rudita.ba@gmail.com>
2019-06-25 08:32:22 +0700

    keDR

--- app/Repositories/DoctorScheduleDetailRepository.php
+++ app/Repositories/DoctorScheduleDetailRepository.php
@@ -6,6 +6,5 @@ interface DoctorScheduleDetailRepository extends BaseRepository
     public function findHistorySchedule($doctor_id, $schedule_id);
     
     public function findSchedule($doctor_id, $schedule_id);
-
-    public function findNextQueue($schedule_id);
+    
 }
